9446df47a3d9aa09a0180c9c91ee16d0726417a5
arobles253 <ar253@rice.edu>
2026-09-19 14:56:07 -0500

    finishing up SPI initialization

--- main.c
+++ main.c
@@ -33,5 +33,25 @@ void rfid_spi_init() {
     //max size of a single data transfer at a time in bytes
     .max_transfer_sz = 32,
   };
+
+  //configuration for SPI slave device connected to SPI buses
+  spi_device_config_t devcfg = {
+    //setting serial clock frequency
+    .clock_speed_hz = 1000000;
+    //defining timing mode, MFRC522 uses SPI Mode 0
+    .mode = 0
+    //defining the chip select pin, allows ESP32 to communicatee specifically with this device
+    .spics_io_num = SDA_PIN,
+    //dicates how many SPI transactions can be running at a time
+    .queue_size = 7,
+  };
+
+  //Initializing SPI bus
+  //specifies which ESP32 internal peripherals to use, uses pointer to assign GPIO pins for bus
+  //Error check will print an error and stop the program if initialization fails
+  ESP_ERROR_CHECK(spi_bus_initialize(HSPI_HOST, &buscfg, SPI_DMA_CH_AUTO));
+  //attaching MFRC522 device to SPI bus, pointer to define settings for this device, pointer to private handle 
+  ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &spi));
+
 }
 
